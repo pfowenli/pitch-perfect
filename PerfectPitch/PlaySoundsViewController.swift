16211013bfcf6f04b3bcde5f4642a054e1780e3a
Owen P. F. Li <pfowenli@gmail.com>
2017-06-17 16:57:01 +0800

    add func performSegue into func audioRecordingDidFinishRecording; set up PlaySoundsViewController

---
+++ PerfectPitch/PlaySoundsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PlaySoundsViewController.swift
+//  PerfectPitch
+//
+//  Created by Owen Pei-Fu LI on 17/06/2017.
+//  Copyright © 2017 UdaCity. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+    
+    var recordedAudioURL: URL!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
